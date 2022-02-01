8c27eb37e5c8ada286a53d724aa79c7a1987e763
Iryusha <Sherbinina_Irina@ukr.net>
2022-02-01 10:41:41 +0200

    fix task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,44 +1,40 @@
 import throttle from 'lodash.throttle';
 
-const formItem = document.querySelector('.feedback-form');
-const textareaItem = document.querySelector('.feedback-form textarea');
-const inputItem = document.querySelector('.feedback-form input');
-
+const formRef = document.querySelector('.feedback-form');
 const STORAGE_KEY = 'feedback-form-state';
+let items = {};
 
-const FormData = {};
+formRef.addEventListener('submit', formSubmit);
+formRef.addEventListener('input', throttle(textAreaInput, 500));
+populateForm();
 
-formItem.addEventListener('submit', FormSubmit);
-formItem.addEventListener('input', throttle(textInput, 500));
+function textAreaInput(event) {
+  items[event.target.name] = event.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(items));
+}
 
-saveInputText();
-
-function FormSubmit(e) {
-  e.preventDefault();
-
-  if (inputItem.value && textareaItem.value) {
-    console.log({
-      email: inputItem.value,
-      message: textareaItem.value,
-    });
-
-    e.currentTarget.reset();
+function formSubmit(event) {
+  event.preventDefault();
+  if (formRef.email.value === '' || formRef.message.value === '') {
+    alert('All fields are required!');
+  } else {
+    console.log(items);
+    event.currentTarget.reset();
     localStorage.removeItem(STORAGE_KEY);
+    items = {};
   }
 }
 
-function textInput(e) {
-  FormData[e.target.name] = e.target.value;
+function populateForm() {
+  const savedInfo = JSON.parse(localStorage.getItem(STORAGE_KEY))
 
-  localStorage.setItem(STORAGE_KEY, JSON.stringify(FormData));
-}
-
-function saveInputText() {
-  const saveText = localStorage.getItem(STORAGE_KEY);
-console.log(saveText)
-  if (saveText) {
-    inputItem.value = JSON.parse(saveText).email || '';
-    textareaItem.value = JSON.parse(saveText).message || '';
+  for (const key in savedInfo) {
+    if (key) {
+      formRef[key].value = savedInfo[key];
+      items = savedInfo;
+    }
   }
 }
+
+
 
